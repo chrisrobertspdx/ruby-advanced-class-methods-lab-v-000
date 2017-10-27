a691c8ce4923f302c7ae54413408f5491a768644
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-27 22:47:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,9 +37,9 @@ class Song
   def self.alphabetical
     @@all.sort{|a,b|
         if a.name < b.name
-          1
-        elsif a.name > b.name
           -1
+        elsif a.name > b.name
+          1
         else
           0
         end
